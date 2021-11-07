26dad2dc2047c93a651befbfb9f9b71da74dc7fe
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-07 22:07:27 +0330

    Update gen.py

--- Bot/plugins/gen.py
+++ Bot/plugins/gen.py
@@ -116,7 +116,7 @@ async def teleGen(strbot, callback_data):
     
     await strbot.send_message(
             chat_id=user_id,
-            text=f"**Here is your Payment String**: \n\n`{session_string}`"
+            text=f"**Here is your Telethon String Session **: \n\n`{session_string}`"
             )
             
     await strbot.send_message(
